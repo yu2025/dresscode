bb3052201490650704f751933568d932b5d34e53
yu2025 <yuki0225shigematsu@gmail.com>
2019-12-24 19:26:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const moment = require('moment');
 const bodyParser = require("body-parser");
+moment.locale('ja');
 const app = express();
 const getScrapBox = require("./utils/getScrapBox");
 const getRandomPair = require("./utils/getRandomPair");
