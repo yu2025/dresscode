8ec8719966e5c7d8c4a0ce683176fb31e5c42af5
yu2025 <yuki0225shigematsu@gmail.com>
2019-12-24 19:25:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const moment = require('moment');
 const bodyParser = require("body-parser");
 const app = express();
 const getScrapBox = require("./utils/getScrapBox");
