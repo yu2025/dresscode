3a2b9c28980416ecb4ff58d795bab4e5e083b0f8
yu2025 <yuki0225shigematsu@gmail.com>
2020-01-01 21:56:46 +0900

    Update upload.js

--- views/gyazo-upload/upload.js
+++ views/gyazo-upload/upload.js
@@ -4,17 +4,17 @@ import test from 'ava'
 import fetchMock from 'fetch-mock'
 import uploadClient from '../../libs/uploadClient'
 
-const imagedata = fs.readFileSync(path.resolve(__dirname, '../fixtures/test-data.txt'), { encoding: 'utf-8' }).trim()
+const imagedata = fs.readFileSync(path.resolve(__dirname, base), { encoding: 'utf-8' }).trim()
 
 fetchMock
   .post('https://upload.gyazo.com/api/upload/easy_auth', {
-    'get_image_url' : 'https://gyazo.com/api/upload/8980c52421e452ac3355ca3e5cfe7a0c',
+    'get_image_url' : 'https://gyazo.com/api/upload/062c5b89c0071595b9a931638e026413733d9a91ecec84c5087d2440c56e5ef0',
     'expires_at' : 1401178164
   })
 
 test('call easy_auth api', (t) => {
   const client = new uploadClient('SAMPLECLIENTID')
   return client.easyAuth(imagedata).then((result) => {
-    t.is(result, 'https://gyazo.com/api/upload/8980c52421e452ac3355ca3e5cfe7a0c')
+    t.is(result, 'https://gyazo.com/api/upload/062c5b89c0071595b9a931638e026413733d9a91ecec84c5087d2440c56e5ef0')
   })
 })
