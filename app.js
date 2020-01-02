cb1dd367918dcc8e5f8f5e7dde5a74e1217d29fe
yu2025 <yuki0225shigematsu@gmail.com>
2020-01-02 21:22:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/judgement",async(req,res)=>{
     
     res.render('search',{
         title:"dresscode",
-        baddress: baddresscode[1].body,
+        baddress: baddresscode[0].h1,
         topsName_0: tops[0].title,
         topsName_1: tops[1].title,
         topsImage: tops[0].image,
